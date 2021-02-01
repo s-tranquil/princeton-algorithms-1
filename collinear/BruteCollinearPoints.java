c0e21871925a31961ee173ac86769d6d61bd8fa5
Mikhail Stepanov <StepanovMM@gmail.com>
2021-02-01 19:46:34 +0300

    collinear: brute force algorithm

---
+++ collinear/BruteCollinearPoints.java
@@ -0,0 +1,66 @@
+import java.util.Arrays;
+
+public class BruteCollinearPoints {
+    private LineSegment[] segments;
+    private int segmentIndex;
+
+    public BruteCollinearPoints(Point[] points) {
+        if (points == null || points.length < 4) {
+            throw new IllegalArgumentException();
+        }
+
+        segmentIndex = 0;
+        segments = new LineSegment[1];
+
+        for (int x1 = 0; x1 < points.length - 3; x1++) {
+            for (int x2 = x1 + 1; x2 < points.length - 2; x2++) {
+                for (int x3 = x2 + 1; x3 < points.length - 1; x3++) {
+                    for (int x4 = x3 + 1; x4 < points.length; x4++) {
+                        Point[] currentSegment = new Point[] {
+                                points[x1], points[x2], points[x3], points[x4]
+                        };
+
+                        double slope12 = currentSegment[0].slopeTo(currentSegment[1]);
+                        double slope13 = currentSegment[0].slopeTo(currentSegment[2]);
+                        double slope14 = currentSegment[0].slopeTo(currentSegment[3]);
+
+                        if (slope12 == slope13 && slope12 == slope14) {
+                            Point min = currentSegment[0];
+                            Point max = currentSegment[0];
+                            for (int k = 1; k < 4; k++) {
+                                if (min.compareTo(currentSegment[k]) > 0) {
+                                    min = currentSegment[k];
+                                }
+                                if (max.compareTo(currentSegment[k]) < 0) {
+                                    max = currentSegment[k];
+                                }
+                            }
+
+                            addSegment(min, max);
+                        }
+                    }
+                }
+            }
+        }
+    }   // finds all line segments containing 4 points
+
+    private void addSegment(Point a, Point b) {
+        if (segmentIndex == segments.length) {
+            LineSegment[] newSegments = new LineSegment[segments.length * 2];
+            for (int i = 0; i < segments.length; i++) {
+                newSegments[i] = segments[i];
+            }
+            segments = newSegments;
+        }
+        segments[segmentIndex] = new LineSegment(a, b);
+        segmentIndex++;
+    }
+
+    public int numberOfSegments() {
+        return segmentIndex;
+    }        // the number of line segments
+
+    public LineSegment[] segments() {
+        return Arrays.copyOf(segments, segmentIndex);
+    }                // the line segments
+}
---
+++ collinear/SampleClient.java
@@ -0,0 +1,46 @@
+/* *****************************************************************************
+ *  Name:
+ *  Date:
+ *  Description:
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+
+public class SampleClient {
+    public static void main(String[] args) {
+
+        // read the n points from a file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        Point[] points = new Point[n];
+        for (int i = 0; i < n; i++) {
+            int x = in.readInt();
+            int y = in.readInt();
+            points[i] = new Point(x, y);
+        }
+
+        // draw the points
+        StdDraw.enableDoubleBuffering();
+        StdDraw.setXscale(0, 32768);
+        StdDraw.setYscale(0, 32768);
+
+        StdDraw.setPenColor(150, 150, 150);
+        StdDraw.setPenRadius(0.01);
+        for (Point p : points) {
+            p.draw();
+        }
+        StdDraw.show();
+
+        StdDraw.setPenColor(100, 100, 255);
+        StdDraw.setPenRadius(0.003);
+        // print and draw the line segments
+        BruteCollinearPoints collinear = new BruteCollinearPoints(points);
+        for (LineSegment segment : collinear.segments()) {
+            StdOut.println(segment);
+            segment.draw();
+        }
+        StdDraw.show();
+    }
+}
